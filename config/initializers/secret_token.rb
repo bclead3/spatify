7e0baca87bc51c4d0480543ddd451197d7a8c605
Ben Leadholm <bclead3@yahoo.com>
2014-01-15 16:05:16 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spatify::Application.config.secret_key_base = '2cba51d5c590482c501778bc30bbd08849d853f27304457a3b8b3c8ecfac391c97ac87361cf0f627f027b22f023b6932705d12458851206b6b77312efbbcbd0e'
